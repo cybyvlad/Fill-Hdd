e8da160ee906c2df0024b805a8988e6f61c039e1
unknown <bav@.(none)>
2010-11-04 10:52:27 +0200

    Initial commit

---
+++ FillHdd/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+
+
+namespace FillHdd
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Wrong arguments");
+                Console.WriteLine("Usage: FillHdd FileThatWillBeUsedToFillHdd NumberOfMBToLeaveFreeOnHardDisk");
+                Console.WriteLine("Example: FillHdd d:\\FillHdd\\data.bin 30");
+                Console.ReadLine();
+                return;
+            }
+            var megabytesToLeaveFree = Convert.ToInt64(args[1])*1024*1024;
+            Fill(args[0], megabytesToLeaveFree);
+        }
+
+
+        public static void Fill(string fileName, long megabytesToLeaveFree)
+        {
+            var fi = new FileInfo(fileName);
+            var root = fi.Directory.Root;
+            var driveInfos = DriveInfo.GetDrives();
+            var driveInfo = driveInfos.Where(x => String.Equals(x.RootDirectory.FullName , root.FullName,StringComparison.OrdinalIgnoreCase)).First();
+            var amountToFill = driveInfo.TotalFreeSpace - megabytesToLeaveFree;
+            using (MemoryMappedFile.CreateFromFile(fileName, FileMode.Create, "tt", amountToFill))
+            {
+            }
+        }
+    }
+}
